b1c96ac59943ff789abee8c5c8bbf8d3e4908bd2
UNOSQUARE\ernesto.cardenas <ernesto.cardenas@unosquare.com>
2022-07-26 15:57:09 -0600

    Initial Changes for theaming

---
+++ LeaveManagment.W3b/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagment.W3b.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
